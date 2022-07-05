b769317adaf680e6dd336fd18b95e6e624e031b9
mrdonald1405 <108753380+mrdonald1405@users.noreply.github.com>
2022-07-05 13:43:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const {LoadCommands, Bot } = require("aoi.js");
+const aoijs = require("aoi.js")
+
+const bot = new aoijs.Bot({
+token: "TOKEN",
+prefix: "!",
+intents: "all",
+})
+
+bot.onMessage();
+bot.onInteractionCreate();
+
+const loader = new aoijs.LoadCommands(bot);
+
+bot.command({
+    name: "$alwaysExecute",
+    code: `
+    $setChannelVar[msg;$getChannelVar[msg]
+    $username: $message]
+    $onlyIF[$isTicket!=false;]
+        `
+})
+
+bot.variables({
+    msg: "",
+    id: "",
+    user: "",
+    category: "",
+    channel: "",
+    setup: "false",
+})
+
+loader.load(bot.cmd,'./cmds/')
